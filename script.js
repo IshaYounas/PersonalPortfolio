3590ea28b5100f362cca4fa802676e1c142e5093
ISHA YOUNAS <G00427804@atu.ie>
2025-05-26 13:50:40 +0100

    added animation to make the portfolio more fun looking for the user

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ document.getElementById("contact-form").addEventListener("submit", function (e)
     e.preventDefault();
     alert("Thank you for contacting me!")
 })
+
+// storing in a nodelist
+const sections = document.querySelectorAll("section");
+
+// creating an observer to watch the sections when user can see them
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) { // section being at least 10% visible
+            entry.target.classList.add("visible"); // making it appear
+        }
+    });
+}, {threshold: 0.1}) // 10% visible
+
+sections.forEach(section => {
+    observer.observe(section) // watch the section
+});
